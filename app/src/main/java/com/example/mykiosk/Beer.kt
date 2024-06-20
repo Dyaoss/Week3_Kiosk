54d31c1f497b3d9c43b3c2a3622abd817f332938
Dyaoss <eogus12003@naver.com>
2024-06-20 20:00:31 +0900

    메뉴의 List정리

--- app/src/main/java/com/example/mykiosk/Beer.kt
+++ app/src/main/java/com/example/mykiosk/Beer.kt
@@ -7,50 +7,34 @@ fun displayBeerMenu() {
     var selectBeer = 0
 
     println("[ Beer MENU ]")
-    var ale = Ale().display()
-    var magpie = Magpie().display()
-    var hand = Hand().display()
+    val beers = listOf(
+        Ale("ShackMeister Ale", 9.8, "쉐이크쉑 버거를 위해 뉴욕 브루클린 브루어리에서 특별히 양조한 에일맥주"),
+        Magpie("Magpie Brewing Co.", 6.8, "Pale Ale Draft, Porter Draft, Wheat Draft"),
+        Hand("The Hand and Malt", 6.8, "IPA Can, Mocha Stout Can")
+    )
+    beers.forEachIndexed { index, beer -> beer.display(index + 1) }
+
     println(" 0. 뒤로가기             | 뒤로가기")
     selectBeer = readLine()!!.toInt()
 
-    when (selectBeer) {
-        1 -> println("${Ale().name}가 선택되었습니다. ")
-        2 -> println("${Magpie().name}가 선택되었습니다. ")
-        3 -> println("${Hand().name}가 선택되었습니다. ")
-        0 -> {
-            main()
-        }
+    if (selectBeer in 0..beers.size - 1) {
+        println("${beers[selectBeer].name}이 선택되었습니다.")
     }
 }
 
-open class Beer() {
-    open fun display() {
-        println("")
+open class Beer(val name: String, val price: Double, val description: String) {
+    open fun display(num: Int) {
+        println("$num.$name | W $price | $description")
     }
 }
 
-class Ale() : Beer() {
-    val name = "ShackMeister Ale"
-    val price = 9.8
-    override fun display() {
-        println(" 1. ${name}   | W ${price} | 쉐이크쉑 버거를 위해 뉴욕 브루클린 브루어리에서 특별히 양조한 에일맥주")
-    }
+class Ale(name: String, price: Double, description: String) : Beer(name, price, description) {
 }
 
-class Magpie() : Beer() {
-    val name = "Magpie Brewing Co."
-    val price = 6.8
-    override fun display() {
-        println(" 2. ${name} | W ${price} | Pale Ale Draft, Porter Draft, Wheat Draft")
-    }
+class Magpie(name: String, price: Double, description: String) : Beer(name, price, description) {
 }
 
-class Hand() : Beer() {
-    val name = "The Hand and Malt"
-    val price = 6.8
-    override fun display() {
-        println(" 3. ${name}  | W ${price} | IPA Can, Mocha Stout Can")
-    }
+class Hand(name: String, price: Double, description: String) : Beer(name, price, description) {
 }
 
 
